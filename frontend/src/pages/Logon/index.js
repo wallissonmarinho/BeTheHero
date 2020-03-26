92f811ff652b2d5d2dab93a1c2b01f8ce7aed723
Wallisson Marinho <wallissonmarinho@hotmail.com>
2020-03-26 16:08:10 -0300

    pagina de login construido

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
 
+import heroesImg from '../../assets/heroes.png';
+import logoImg from '../../assets/logo.svg';
+
 function Logon() {
-  return <h1>Hello world</h1>;
+  return (
+    <div className='logon-container'>
+      <section className='form'>
+        <img src={logoImg} alt='Be The Hero' />
+
+        <form>
+          <h1>Faça seu logo</h1>
+
+          <input placeholder='Sua ID' />
+          <button className='button' type='submit'>
+            Entrar
+          </button>
+
+          <a href='/register'>
+            <FiLogIn size={16} color='#e02041' />
+            Não tenho cadastro
+          </a>
+        </form>
+      </section>
+
+      <img src={heroesImg} alt='Heroes' />
+    </div>
+  );
 }
 
 export default Logon;
